ee3f115bc1571d272aed3047b45e442bfa61fa71
Filipe Campos <filipepcampos24@gmail.com>
2020-03-30 09:16:41 +0100

    Update #3

--- cmake-build-debug/CMakeFiles/oware.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/oware.dir/cmake_clean.cmake
@@ -1,11 +1,10 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/oware.dir/src/main.cpp.o"
   "CMakeFiles/oware.dir/src/Board.cpp.o"
   "CMakeFiles/oware.dir/src/Player.cpp.o"
   "CMakeFiles/oware.dir/src/Tcp.cpp.o"
-  "CMakeFiles/oware.dir/src/main.cpp.o"
-  "liboware.dll.a"
-  "oware.exe"
   "oware.pdb"
+  "oware"
 )
 
 # Per-language clean rules from dependency scanning.
